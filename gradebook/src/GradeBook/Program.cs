f50cf92bcf4c0e984ab74d088fc6db7369776599
Gerhard Molin <gerhard.molin@schoolday.com>
2020-10-02 12:26:02 +0300

    learning switch cases in CSharp and error catching

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,20 +8,31 @@ namespace GradeBook
 		static void Main(string[] args)
 		{
 			var newBook = new Book("Gary's Book");
-
-			newBook.AddGrade(43.1);
-			newBook.AddGrade(31.1);
-			newBook.AddGrade(54.1);
+			while (true)
+			{
+				Console.WriteLine("Enter a grade or 'q' to quit");
+				var input = Console.ReadLine();
+				if (input == "q")
+				{
+					break;
+				}
+				try 
+				{
+					var grade = double.Parse(input);
+					newBook.AddGrade(grade);
+				}
+				catch (ArgumentException ex) 
+				{
+					Console.WriteLine(ex.Message);
+				}
+				catch (FormatException ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+			}
 			var stats = newBook.getStatistics();
-			Console.WriteLine($"The average grade is {stats.Average:N1}.\nThe lowerst grade is: {stats.Low}.\nThe highest grade is: {stats.High}");
-			if (args.Length > 0) 
-			{
-				Console.WriteLine("Hello, " + args[0] + "!");
-			}
-			else 
-			{
-				Console.Write("Usage: ./dotnet run <name>");
-			}
+			Console.WriteLine($"The average grade is {stats.Average:N1}.\nThe lowest grade is: {stats.Low}.\nThe highest grade is: {stats.High}");
+			Console.WriteLine($"The letter grade is {stats.Letter}");
 		}
 	}
 }
